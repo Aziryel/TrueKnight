c2529c20d4065bfb83ddcd5625f4535135180213
Rafa <divinewarrioraziryel@gmail.com>
2024-05-10 13:41:43 +0200

    Base Character classes

---
+++ Source/TrueKnight/Public/Character/TKEnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/TKCharacterBase.h"
+#include "TKEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRUEKNIGHT_API ATKEnemyCharacter : public ATKCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TrueKnight/Public/Character/TKPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/TKCharacterBase.h"
+#include "TKPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRUEKNIGHT_API ATKPlayerCharacter : public ATKCharacterBase
+{
+	GENERATED_BODY()
+	
+};
