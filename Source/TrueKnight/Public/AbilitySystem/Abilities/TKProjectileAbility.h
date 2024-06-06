2906dbe0434d1bf146c128a3fce54713aa3fbd9f
Rafa <divinewarrioraziryel@gmail.com>
2024-06-06 14:11:53 +0200

    Implemented initial functionality for the Projectile Ability base class

---
+++ Source/TrueKnight/Public/AbilitySystem/Abilities/TKProjectileAbility.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/TKGameplayAbility.h"
+#include "TKProjectileAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRUEKNIGHT_API UTKProjectileAbility : public UTKGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
